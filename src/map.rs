18d2d2905b0df6cc2fbe09863759a42820e11a87
Nicholas Fleck <TheBerkin@users.noreply.github.com>
2020-09-10 19:22:49 -0500

    Add more map methods

--- src/map.rs
+++ src/map.rs
@@ -1,8 +1,14 @@
-use std::{collections::{HashMap, HashSet, hash_map::RandomState}, hash::BuildHasher, hash::Hash, borrow::Borrow, borrow::BorrowMut};
+use std::{
+  borrow::Borrow,
+  collections::{hash_map::RandomState, HashSet},
+  hash::{Hash, BuildHasher},
+  ops::Index
+};
 
-use indexmap::IndexMap;
-use smallvec::{SmallVec, smallvec};
+use indexmap::{IndexMap};
+use smallvec::{smallvec, SmallVec};
 
+#[derive(Clone)]
 pub struct ScopeMap<K, V, S: BuildHasher = RandomState> {
   map: IndexMap<K, SmallVec<[V; 1]>, S>,
   layers: SmallVec<[HashSet<usize>; 1]>,
@@ -15,15 +21,34 @@ impl<K, V, S: Default + BuildHasher> Default for ScopeMap<K, V, S> {
   }
 }
 
+impl<K, Q: ?Sized, V, S> Index<&Q> for ScopeMap<K, V, S>
+where 
+  K: Eq + Hash + Borrow<Q>,
+  Q: Eq + Hash,
+  S: BuildHasher,
+{
+  type Output = V;
+
+  /// Returns a reference to the value associated with the provided key.
+  ///
+  /// # Panics
+  ///
+  /// Panics if the key does not exist in the `ScopeMap`.
+  #[inline]
+  fn index(&self, index: &Q) -> &Self::Output {
+    self.get(index).expect("key not found in map")
+  }
+}
+
 impl<K, V> ScopeMap<K, V, RandomState> {
   #[inline]
   pub fn new() -> ScopeMap<K, V, RandomState> {
     Self {
       map: Default::default(),
-      layers: smallvec![Default::default()]
+      layers: smallvec![Default::default()],
     }
   }
-
+  
   #[inline]
   pub fn with_capacity(capacity: usize) -> ScopeMap<K, V, RandomState> {
     Self::with_capacity_and_hasher(capacity, Default::default())
@@ -38,7 +63,7 @@ impl<K, V, S: BuildHasher> ScopeMap<K, V, S> {
       layers: smallvec![Default::default()],
     }
   }
-
+  
   #[inline]
   pub fn with_capacity_and_hasher(capacity: usize, hash_builder: S) -> Self {
     Self {
@@ -46,96 +71,180 @@ impl<K, V, S: BuildHasher> ScopeMap<K, V, S> {
       layers: smallvec![Default::default()],
     }
   }
-
+  
   #[inline]
   pub fn capacity(&self) -> usize {
     self.map.capacity()
   }
 }
 
-impl<K, V> ScopeMap<K, V> {
+impl<K, V, S> ScopeMap<K, V, S> 
+where 
+  K: Eq + Hash,
+  S: BuildHasher,
+{
+  /// Returns `true` if the map has no keys.
   #[inline]
   pub fn is_empty(&self) -> bool {
     self.map.is_empty()
   }
-
+  
+  /// Gets the number of distinct keys throughout all layers.
   #[inline]
   pub fn len(&self) -> usize {
     self.map.len()
   }
-
+  
+  /// Gets the number of active layers.
   #[inline]
-  pub fn scope_count(self) -> usize {
+  pub fn layer_count(self) -> usize {
     self.layers.len()
   }
-
+  
+  /// Adds a new, empty layer.
   #[inline]
-  pub fn add_layer(&mut self) {
+  pub fn push_layer(&mut self) {
     self.layers.push(Default::default())
   }
-
+  
+  /// Removes the topmost layer, if there is more than one.
+  /// Returns `true` if a layer was removed.
   #[inline]
-  pub fn remove_layer(&mut self) -> bool {
+  pub fn pop_layer(&mut self) -> bool {
     if self.layers.len() > 1 {
       for stack_index in self.layers.pop().unwrap() {
         if let Some((_key, stack)) = self.map.get_index_mut(stack_index) {
           stack.pop();
         }
       }
-      return true
+      return true;
     }
     false
   }
 }
 
-impl<K: Eq + Hash, V> ScopeMap<K, V> {
-  pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<&V>
-  where 
+impl<K: Eq + Hash, V, S: BuildHasher> ScopeMap<K, V, S> {
+  
+  /// Returns `true` if the map contains the specified key in any layer.
+  #[inline]
+  pub fn contains_key<Q: ?Sized>(&self, key: &Q) -> bool
+  where
     K: Borrow<Q>,
     Q: Eq + Hash,
   {
+    self.map.contains_key(key)
+  } 
+
+  /// Returns `true` if the map contains the specified key at the top layer.
+  #[inline]
+  pub fn contains_key_at_top<Q: ?Sized>(&self, key: &Q) -> bool
+  where
+    K: Borrow<Q>,
+    Q: Eq + Hash,
+  {
+    self.map.get_full(key).map_or(false, |(index, ..)| self.layers.last().unwrap().contains(&index))
+  }
+  
+  /// Gets a reference to the topmost value associated with a key.
+  #[inline]
+  pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<&V>
+  where
+  K: Borrow<Q>,
+  Q: Eq + Hash,
+  {
     self.map.get(key).and_then(|v| v.last())
   }
-
+  
+  /// Gets a mutable reference to the topmost value associated with a key.
+  #[inline]
   pub fn get_mut<Q: ?Sized>(&mut self, key: &Q) -> Option<&mut V>
-  where 
-    K: BorrowMut<Q>,
-    Q: Eq + Hash,
+  where
+  K: Borrow<Q>,
+  Q: Eq + Hash,
   {
     self.map.get_mut(key).and_then(|v| v.last_mut())
   }
-
+  
+  /// Gets a reference to a value `skip_count` layers below the topmost value associated with a key.
   #[inline]
-  pub fn insert(&mut self, key: K, value: V) {
-    // Check if the key exists and is in the current layer
-    if self.map.contains_key(&key) && self.layers.last().map_or(false, |layer| layer.contains(&self.map.get_index_of(&key).unwrap())) {
-      // Key exists in current layer
-      if let Some(top) = self.map.get_mut(&key).and_then(|stack| stack.last_mut()) {
-        *top = value;
-      }
+  pub fn get_parent<Q: ?Sized>(&self, key: &Q, skip_count: usize) -> Option<&V>
+  where
+  K: Borrow<Q>,
+  Q: Eq + Hash,
+  {
+    if let Some((stack_index, _key, stack)) = self.map.get_full(key) {
+      // If the skip count exceeds the stack size, it shouldn't matter because take() is self-truncating
+      let stack_skip_count = self
+      .layers
+      .iter()
+      .rev()
+      .take(skip_count)
+      .filter(|layer| layer.contains(&stack_index))
+      .count();
+      return stack.iter().rev().nth(stack_skip_count)
+    }
+    None
+  }
+  
+  /// Gets a mutable reference to a value `skip_count` layers below the topmost value associated with a key.
+  #[inline]
+  pub fn get_parent_mut<Q: ?Sized>(&mut self, key: &Q, skip_count: usize) -> Option<&mut V>
+  where
+    K: Borrow<Q>,
+    Q: Eq + Hash,
+  {
+    if let Some((stack_index, _key, stack)) = self.map.get_full_mut(key) {
+      // If the skip count exceeds the stack size, it shouldn't matter because take() is self-truncating
+      let stack_skip_count = self
+      .layers
+      .iter()
+      .rev()
+      .take(skip_count)
+      .filter(|layer| layer.contains(&stack_index))
+      .count();
+      return stack.iter_mut().rev().nth(stack_skip_count)
+    }
+    None
+  }
+  
+  /// Adds a value to the topmost layer.
+  #[inline]
+  pub fn define(&mut self, key: K, value: V) {
+    let entry = self.map.entry(key);
+    let stack_index = entry.index();
+    let stack = entry.or_insert_with(Default::default);    
+    let is_new = self.layers.last_mut().unwrap().insert(stack_index);
+    
+    if is_new {
+      stack.push(value);
     } else {
-      // Key doesn't exist in current layer
-
-      // First check that the map has a stack for this key
-      let stack_index = if let Some((stack_index, _key, stack)) = self.map.get_full_mut(&key) {
-        // Push the value onto the stack for the key
-        stack.push(value);
-        stack_index
-      } else {
-        // If there's no stack, make one and give back its index in the map
-        self.map.insert_full(key, smallvec![value]).0
-      };
-      // Add the key to the key set for the current layer
-      self.layers.last_mut().unwrap().insert(stack_index);
+      *stack.last_mut().unwrap() = value;
     }
   }
-
+  
+  /// Removes a value from the topmost layer.
   #[inline]
-  pub fn remove(&mut self, key: K) {
+  pub fn delete(&mut self, key: K) {
     if let Some((index, _key, stack)) = self.map.get_full_mut(&key) {
       if self.layers.last_mut().unwrap().remove(&index) {
         stack.pop();
       }
     }
+  }
+  
+  /// Removes all entries in the topmost layer.
+  #[inline]
+  pub fn clear_top(&mut self) {
+    for stack_index in self.layers.last_mut().unwrap().drain() {
+      self.map.get_index_mut(stack_index).unwrap().1.pop();
+    }
+  }
+  
+  /// Removes all elements and additional layers.
+  #[inline]
+  pub fn clear_all(&mut self) {
+    self.map.clear();
+    self.layers.clear();
+    self.layers.push(Default::default())
   }
 }
